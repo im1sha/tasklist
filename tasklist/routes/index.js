3063de29f225ab51c12eb28743332eb03f6cee13
im1sha <vkporsche@ya.ru>
2019-02-24 23:10:57 +0300

    Task item element : partial implementation

--- tasklist/routes/index.js
+++ tasklist/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+
+
 var router = express.Router();
+
+const totalTasks = 5;
+const newText  = {
+  title : "Task",
+  state : "Completed",
+  date : "Date",
+  loadedFile : "",
+};
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('task', {
+    title: newText.title,
+    state: newText.state,
+    date: newText.date,
+    loadedFile : newText.loadedFile,
+    emailsVisible: true,
+    emails: ["1@gmail.com", "2@gmail.com"],
+    phone: "+1234567890",
+  });
 });
 
 module.exports = router;
